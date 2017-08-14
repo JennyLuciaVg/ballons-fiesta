0851cc4e42026a58190f4b352e00e50cd64bb49a
Jenny Vega <jelusita11@gmail.com>
2017-08-13 21:36:18 -0500

    index view almost finished. Enhance responsive

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var express = require('express');
-var stylus = require('stylus');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var stylus = require('stylus');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -22,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(stylus.middleware(path.join(__dirname, 'public')));
+//app.use(stylus.middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
